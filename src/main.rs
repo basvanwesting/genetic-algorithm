d31034743f2dd631b52864986fb4c0715ea8e168
basvanwesting <basvanwesting@hotmail.com>
2022-04-12 15:45:02 +0200

    Add main.rb with basic performance testing setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use genetic_algorithm::context::Context;
+use genetic_algorithm::evolve;
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::new()
+        .with_gene_size(100)
+        .with_population_size(1000)
+        .with_tournament_size(4)
+        .with_max_stale_generations(20);
+
+    let best_chromosome = evolve::call(&context);
+    println!("{:#?}", best_chromosome);
 }
