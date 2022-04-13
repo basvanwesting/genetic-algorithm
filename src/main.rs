a5877a9aec16a5943c0030d99b0ac34bc1ced78a
basvanwesting <basvanwesting@hotmail.com>
2022-04-13 17:39:51 +0200

    Add run for u8 next to bool in main

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,21 @@ fn main() {
     } else {
         println!("no best chromosome");
     }
+
+    let context = Context::<u8>::new()
+        .with_gene_size(100)
+        .with_gene_values(vec![Gene(1), Gene(2), Gene(3), Gene(4)])
+        .with_population_size(1000)
+        .with_tournament_size(4)
+        .with_max_stale_generations(20)
+        .with_mutation_probability(0.1)
+        .with_fitness_function(fitness::sum_values);
+
+    println!("{}", context);
+
+    if let Some(best_chromosome) = evolve::call(&context) {
+        println!("best chromosome: {}", best_chromosome);
+    } else {
+        println!("no best chromosome");
+    }
 }
