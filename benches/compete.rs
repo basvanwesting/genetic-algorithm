4c2a02335da05b76df7172bfd474f46ac0411de3
basvanwesting <basvanwesting@hotmail.com>
2022-04-23 08:33:54 +0200

    fix benches

--- benches/compete.rs
+++ benches/compete.rs
@@ -2,30 +2,39 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
 use genetic_algorithm::compete;
 use genetic_algorithm::compete::Compete;
-use genetic_algorithm::genotype::Genotype;
 use genetic_algorithm::fitness;
 use genetic_algorithm::fitness::Fitness;
+use genetic_algorithm::genotype::Genotype;
+use genetic_algorithm::population::Population;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let gen_genotype = Genotype::new()
-        .with_gene_size(11)
+    let source_population_size = 2000;
+    let target_population_size = 1000;
+    let mut rng = SmallRng::from_entropy();
+    let genotype = Genotype::new()
+        .with_gene_size(10)
         .with_gene_values(vec![true, false]);
-    let population = gen_genotype.permutation_population_factory();
-    let population = fitness::SimpleSum.call_for_population(population);
 
-    let genotype = Genotype::new().with_population_size(1024);
+    let chromosomes = (0..source_population_size)
+        .map(|_| genotype.random_chromosome_factory(&mut rng))
+        .collect();
+
+    let population = Population::new(chromosomes);
+    let population = fitness::SimpleSum.call_for_population(population);
 
     println!(
         "start population size: {}, target population size: {}",
         population.size(),
-        genotype.population_size,
+        target_population_size,
     );
 
     c.bench_function("compete_tournament", |b| {
         let compete = compete::Tournament(4);
         b.iter_batched(
             || population.clone(),
-            |data| compete.call(&genotype, data),
+            |data| compete.call(&genotype, data, target_population_size, &mut rng),
             BatchSize::SmallInput,
         )
     });
@@ -34,7 +43,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let compete = compete::Elite;
         b.iter_batched(
             || population.clone(),
-            |data| compete.call(&genotype, data),
+            |data| compete.call(&genotype, data, target_population_size, &mut rng),
             BatchSize::SmallInput,
         )
     });
--- benches/crossover.rs
+++ benches/crossover.rs
@@ -1,22 +1,31 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
-use genetic_algorithm::genotype::Genotype;
 use genetic_algorithm::crossover;
 use genetic_algorithm::crossover::Crossover;
+use genetic_algorithm::genotype::Genotype;
+use genetic_algorithm::population::Population;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = SmallRng::from_entropy();
     let genotype = Genotype::new()
         .with_gene_size(100)
-        .with_population_size(1000)
         .with_gene_values(vec![true, false]);
-    let population = genotype.random_population_factory();
+
+    let chromosomes = (0..1000)
+        .map(|_| genotype.random_chromosome_factory(&mut rng))
+        .collect();
+
+    let population = Population::new(chromosomes);
+
     println!("population size: {}", population.size());
 
     c.bench_function("crossover_individual", |b| {
         let crossover = crossover::Individual(false);
         b.iter_batched(
             || population.clone(),
-            |data| crossover.call(&genotype, data),
+            |data| crossover.call(&genotype, data, &mut rng),
             BatchSize::SmallInput,
         )
     });
@@ -25,7 +34,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let crossover = crossover::All(false);
         b.iter_batched(
             || population.clone(),
-            |data| crossover.call(&genotype, data),
+            |data| crossover.call(&genotype, data, &mut rng),
             BatchSize::SmallInput,
         )
     });
@@ -34,7 +43,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let crossover = crossover::Range(false);
         b.iter_batched(
             || population.clone(),
-            |data| crossover.call(&genotype, data),
+            |data| crossover.call(&genotype, data, &mut rng),
             BatchSize::SmallInput,
         )
     });
