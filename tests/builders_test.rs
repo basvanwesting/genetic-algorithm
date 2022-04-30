ff8d7c9d3cdf99f710392dad2a1e74056e9c64a3
basvanwesting <basvanwesting@hotmail.com>
2022-04-30 07:57:09 +0200

    Enable all remaining tests

--- tests/builders_test.rs
+++ tests/builders_test.rs
@@ -3,10 +3,12 @@ mod support;
 #[cfg(test)]
 mod builders_tests {
     use crate::support::*;
+    use genetic_algorithm::gene::{ContinuousGene, DiscreteGene};
+    use genetic_algorithm::genotype::{BinaryGenotype, ContinuousGenotype, DiscreteGenotype};
 
     #[test]
     fn test_chromosome_binary() {
-        let chromosome = build::chromosome(vec![true, false, true, false]);
+        let chromosome = build::chromosome::<BinaryGenotype>(vec![true, false, true, false]);
         println!("{:#?}", chromosome);
         assert_eq!(
             inspect::chromosome(&chromosome),
@@ -15,22 +17,30 @@ mod builders_tests {
     }
 
     #[test]
-    fn test_chromosome_discrete() {
-        let chromosome = build::chromosome(vec![3, 4, 5, 6]);
+    fn test_chromosome_discrete_discrete() {
+        let chromosome = build::chromosome::<DiscreteGenotype<DiscreteGene>>(vec![3, 4, 5, 6]);
         println!("{:#?}", chromosome);
         assert_eq!(inspect::chromosome(&chromosome), vec![3, 4, 5, 6]);
     }
 
     #[test]
+    fn test_chromosome_discrete_continuous() {
+        let chromosome =
+            build::chromosome::<DiscreteGenotype<ContinuousGene>>(vec![0.3, 0.4, 0.5, 0.6]);
+        println!("{:#?}", chromosome);
+        assert_eq!(inspect::chromosome(&chromosome), vec![0.3, 0.4, 0.5, 0.6]);
+    }
+
+    #[test]
     fn test_chromosome_continuous() {
-        let chromosome = build::chromosome(vec![0.1, 0.2, 0.3]);
+        let chromosome = build::chromosome::<ContinuousGenotype>(vec![0.1, 0.2, 0.3]);
         println!("{:#?}", chromosome);
         assert_eq!(inspect::chromosome(&chromosome), vec![0.1, 0.2, 0.3]);
     }
 
     #[test]
     fn test_population_binary() {
-        let population = build::population(vec![
+        let population = build::population::<BinaryGenotype>(vec![
             vec![true, true, true],
             vec![true, true, false],
             vec![true, false, false],
