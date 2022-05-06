8a928525ae14323d2270f143c7b194ded7fe8aac
basvanwesting <basvanwesting@hotmail.com>
2022-05-06 13:06:24 +0200

    Add tests for CompeteTournament with none fitness values

--- tests/compete/tournament_test.rs
+++ tests/compete/tournament_test.rs
@@ -93,3 +93,51 @@ fn minimize_population_shortage() {
         vec![vec![false, false, false], vec![false, false, true],]
     );
 }
+
+#[test]
+fn minimize_population_surplus_with_none_fitness() {
+    let population = build::population_with_fitness_scores::<BinaryGenotype>(vec![
+        (vec![false, false, false], Some(0)),
+        (vec![false, false, true], None),
+        (vec![false, true, false], Some(1)),
+        (vec![false, true, true], Some(2)),
+        (vec![true, false, false], Some(1)),
+        (vec![true, false, true], Some(2)),
+        (vec![true, true, false], Some(2)),
+        (vec![true, true, true], Some(3)),
+    ]);
+
+    let mut rng = SmallRng::seed_from_u64(0);
+    let population = CompeteTournament(4).call(population, FitnessOrdering::Minimize, 4, &mut rng);
+
+    assert_eq!(
+        inspect::population_with_fitness_scores(&population),
+        vec![
+            (vec![false, false, false], Some(0)),
+            (vec![false, true, false], Some(1)),
+            (vec![true, false, false], Some(1)),
+            (vec![true, false, true], Some(2)),
+        ]
+    );
+}
+
+#[test]
+fn minimize_population_shortage_with_none_fitness() {
+    let population = build::population_with_fitness_scores::<BinaryGenotype>(vec![
+        (vec![false, false, false], None),
+        (vec![false, false, true], Some(1)),
+        (vec![false, true, true], Some(2)),
+    ]);
+
+    let mut rng = SmallRng::seed_from_u64(0);
+    let population = CompeteTournament(4).call(population, FitnessOrdering::Minimize, 4, &mut rng);
+
+    assert_eq!(
+        inspect::population_with_fitness_scores(&population),
+        vec![
+            (vec![false, false, true], Some(1)),
+            (vec![false, true, true], Some(2)),
+            (vec![false, false, false], None),
+        ]
+    );
+}
