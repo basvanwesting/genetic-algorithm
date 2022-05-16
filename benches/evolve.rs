fd5dc5eb7d2bba9c4f7a9a7262e934aed8c79838
basvanwesting <basvanwesting@hotmail.com>
2022-05-16 10:49:35 +0200

    Add benchmark for Evolve, use Zero fitness placeholder to ensure duration is always max_stale_generations

---
+++ benches/evolve.rs
@@ -0,0 +1,79 @@
+use criterion::*;
+use genetic_algorithm::evolve::prelude::*;
+use genetic_algorithm::fitness::placeholders::Zero;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
+use std::time::Duration;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = SmallRng::from_entropy();
+    let gene_size = 100;
+    let population_size = 100;
+    let max_stale_generations = 100;
+
+    let mut group = c.benchmark_group("evolve");
+    group.warm_up_time(Duration::from_secs(5));
+    group.measurement_time(Duration::from_secs(10));
+    //let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+    //group.plot_config(plot_config);
+
+    group.bench_function(
+        format!(
+            "binary-{}-pop{}-gen{}",
+            gene_size, population_size, max_stale_generations
+        ),
+        |b| {
+            let genotype = BinaryGenotype::builder()
+                .with_gene_size(gene_size)
+                .build()
+                .unwrap();
+
+            let evolve_builder = Evolve::builder()
+                .with_genotype(genotype)
+                .with_population_size(population_size)
+                .with_max_stale_generations(max_stale_generations)
+                .with_mutate(MutateOnce(0.2))
+                .with_fitness(Zero::new())
+                .with_crossover(CrossoverSingle(true))
+                .with_compete(CompeteTournament(4));
+
+            b.iter_batched(
+                || evolve_builder.clone().build().unwrap(),
+                |e| e.call(&mut rng),
+                BatchSize::SmallInput,
+            );
+        },
+    );
+
+    group.bench_function(
+        format!(
+            "discrete-{}-pop{}-gen{}",
+            gene_size, population_size, max_stale_generations
+        ),
+        |b| {
+            let genotype = DiscreteGenotype::builder()
+                .with_gene_size(gene_size)
+                .with_gene_values((0..10).collect())
+                .build()
+                .unwrap();
+
+            let evolve_builder = Evolve::builder()
+                .with_genotype(genotype)
+                .with_population_size(population_size)
+                .with_max_stale_generations(max_stale_generations)
+                .with_mutate(MutateOnce(0.2))
+                .with_fitness(Zero::new())
+                .with_crossover(CrossoverSingle(true))
+                .with_compete(CompeteTournament(4));
+
+            b.iter_batched(
+                || evolve_builder.clone().build().unwrap(),
+                |e| e.call(&mut rng),
+                BatchSize::SmallInput,
+            );
+        },
+    );
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
