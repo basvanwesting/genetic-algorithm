ea7a7131626cba6bae68b32734fe3445eb1c6f68
basvanwesting <basvanwesting@hotmail.com>
2022-06-29 12:14:25 +0200

    Add hill_climb_milp example

---
+++ examples/hill_climb_milp.rs
@@ -0,0 +1,67 @@
+use genetic_algorithm::strategy::hill_climb::prelude::*;
+use rand::prelude::*;
+use rand::rngs::SmallRng;
+
+const LP: f32 = 59.0;
+const PRECISION: f32 = 1e-5;
+
+// see https://www.mathworks.com/help/optim/ug/intlinprog.html#bts3gkc-2
+#[derive(Clone, Debug)]
+struct MILPFitness;
+impl Fitness for MILPFitness {
+    type Genotype = MultiContinuousGenotype;
+    fn calculate_for_chromosome(
+        &mut self,
+        chromosome: &Chromosome<Self::Genotype>,
+    ) -> Option<FitnessValue> {
+        let x1 = chromosome.genes[0];
+        let x2 = chromosome.genes[1].floor();
+
+        if x1 + 2.0 * x2 >= 14.0 && -4.0 * x1 - x2 <= 33.0 && 2.0 * x1 + x2 <= 20.0 {
+            let score = (8.0 * x1 + x2 - LP).abs();
+            Some((score / PRECISION) as isize)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let mut rng = SmallRng::from_entropy();
+    let genotype = MultiContinuousGenotype::builder()
+        .with_allele_multi_range(vec![(-10.0..10.0), (0.0..10.0)])
+        .with_allele_multi_neighbour_range(vec![(-1.0..1.0), (-1.0..1.0)])
+        .build()
+        .unwrap();
+
+    let hill_climb_builder = HillClimb::builder()
+        .with_genotype(genotype)
+        .with_max_stale_generations(100000)
+        .with_target_fitness_score(0)
+        .with_fitness_ordering(FitnessOrdering::Minimize)
+        .with_fitness(MILPFitness);
+
+    for _ in 0..10 {
+        let now = std::time::Instant::now();
+        let hill_climb = hill_climb_builder.clone().call(&mut rng).unwrap();
+        let duration = now.elapsed();
+
+        if let Some(best_chromosome) = hill_climb.best_chromosome() {
+            if let Some(fitness_score) = best_chromosome.fitness_score {
+                let x1 = best_chromosome.genes[0];
+                let x2 = best_chromosome.genes[1].floor();
+                let result = 8.0 * x1 + x2;
+
+                println!(
+                    "x1: {:.5}, x2: {} = {:.5} (fitness score: {: >5}, duration {:?})",
+                    x1, x2 as u8, result, fitness_score, duration
+                );
+            } else {
+                println!(
+                    "invalid solution with fitness score: none, duration {:?}",
+                    duration
+                );
+            }
+        }
+    }
+}
