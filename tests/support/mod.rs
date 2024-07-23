77ddef8bf03f5b09a8fca6e363edf96a0311f7e6
basvanwesting <basvanwesting@hotmail.com>
2024-07-23 11:22:24 +0200

    remove unused_imports warning in test support

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -1,6 +1,9 @@
 pub mod build;
 pub mod inspect;
 
+#[allow(unused_imports)]
 pub use num::BigUint;
+#[allow(unused_imports)]
 pub use rand::rngs::SmallRng;
+#[allow(unused_imports)]
 pub use rand::SeedableRng;
