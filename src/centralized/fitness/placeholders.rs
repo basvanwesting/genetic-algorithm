edf74dac07d96d95b46363ca55f63b08b9aa189a
basvanwesting <basvanwesting@hotmail.com>
2025-09-09 16:01:42 +0200

    trim GenesOwner based placeholders in centralized/fitness/placeholders

--- src/centralized/fitness/placeholders.rs
+++ src/centralized/fitness/placeholders.rs
@@ -1,7 +1,6 @@
 //! placeholders for testing and bootstrapping, not really used in practice
 use crate::centralized::allele::RangeAllele;
-use crate::centralized::chromosome::GenesOwner;
-use crate::centralized::fitness::{Fitness, FitnessChromosome, FitnessPopulation, FitnessValue};
+use crate::centralized::fitness::{Fitness, FitnessPopulation, FitnessValue};
 use crate::centralized::genotype::{
     DynamicRangeGenotype, Genotype, StaticBinaryGenotype, StaticRangeGenotype,
 };
@@ -12,30 +11,6 @@ use rand::SeedableRng;
 use std::marker::PhantomData;
 use std::ops::Range;
 use std::{thread, time};
-
-/// placeholder for testing and bootstrapping, not really used in practice
-#[derive(Clone, Debug)]
-pub struct Zero<G: Genotype>(PhantomData<G>);
-impl<G: Genotype> Zero<G> {
-    pub fn new() -> Self {
-        Self(PhantomData)
-    }
-}
-impl<G: Genotype> Default for Zero<G> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-impl<G: Genotype> Fitness for Zero<G> {
-    type Genotype = G;
-    fn calculate_for_chromosome(
-        &mut self,
-        _chromosome: &FitnessChromosome<Self>,
-        _genotype: &Self::Genotype,
-    ) -> Option<FitnessValue> {
-        Some(0)
-    }
-}
 
 /// placeholder for testing and bootstrapping, not really used in practice
 /// Static version of Zero that works with population-based calculation
@@ -145,56 +120,6 @@ impl<const N: usize, const M: usize> Clone for CountStaticTrueWithSleep<N, M> {
             micro_seconds: self.micro_seconds,
             print_on_clone: self.print_on_clone,
         }
-    }
-}
-
-/// placeholder for testing and bootstrapping, not really used in practice
-/// Sums the genes and converts to [FitnessValue]
-/// There are 2 constructors:
-/// * new(), precision is defaulted to 1.0
-/// * new_with_precision(precision)
-#[derive(Clone, Debug)]
-pub struct SumGenes<G: Genotype> {
-    precision: f64,
-    _phantom: PhantomData<G>,
-}
-impl<G: Genotype> SumGenes<G> {
-    pub fn new() -> Self {
-        Self::default()
-    }
-    pub fn new_with_precision(precision: f64) -> Self {
-        Self {
-            precision,
-            ..Default::default()
-        }
-    }
-}
-impl<G: Genotype> Default for SumGenes<G> {
-    fn default() -> Self {
-        Self {
-            precision: 1.0_f64,
-            _phantom: PhantomData,
-        }
-    }
-}
-impl<G: Genotype> Fitness for SumGenes<G>
-where
-    G::Allele: Into<f64>,
-    G::Genes: IntoIterator<Item = G::Allele>,
-    G::Chromosome: GenesOwner<Genes = G::Genes>,
-{
-    type Genotype = G;
-    fn calculate_for_chromosome(
-        &mut self,
-        chromosome: &FitnessChromosome<Self>,
-        _genotype: &Self::Genotype,
-    ) -> Option<FitnessValue> {
-        let sum: f64 = chromosome
-            .genes()
-            .clone()
-            .into_iter()
-            .fold(0.0_f64, |acc, e| acc + e.into());
-        Some((sum / self.precision) as FitnessValue)
     }
 }
 
@@ -324,68 +249,6 @@ where
             })
             .map(Some)
             .collect()
-    }
-}
-
-/// placeholder for testing and bootstrapping, not really used in practice
-#[derive(Clone, Debug)]
-pub struct Countdown<G: Genotype>(usize, PhantomData<G>);
-impl<G: Genotype> Countdown<G> {
-    pub fn new(start: usize) -> Self {
-        Self(start, PhantomData)
-    }
-}
-impl<G: Genotype> Fitness for Countdown<G> {
-    type Genotype = G;
-    fn calculate_for_chromosome(
-        &mut self,
-        _chromosome: &FitnessChromosome<Self>,
-        _genotype: &Self::Genotype,
-    ) -> Option<FitnessValue> {
-        if self.0 == 0 {
-            Some(0)
-        } else {
-            self.0 -= 1;
-            Some(self.0 as FitnessValue)
-        }
-    }
-}
-
-/// placeholder for testing and bootstrapping, not really used in practice
-#[derive(Clone, Debug)]
-pub struct CountdownNoisy<G: Genotype> {
-    start: usize,
-    step: usize,
-    noise_sampler: Uniform<usize>,
-    rng: SmallRng,
-    _phantom: PhantomData<G>,
-}
-impl<G: Genotype> CountdownNoisy<G> {
-    pub fn new(start: usize, step: usize, noise_range: Range<usize>) -> Self {
-        Self {
-            start,
-            step,
-            noise_sampler: Uniform::from(noise_range),
-            rng: SmallRng::seed_from_u64(0),
-            _phantom: PhantomData,
-        }
-    }
-}
-impl<G: Genotype> Fitness for CountdownNoisy<G> {
-    type Genotype = G;
-    fn calculate_for_chromosome(
-        &mut self,
-        _chromosome: &FitnessChromosome<Self>,
-        _genotype: &Self::Genotype,
-    ) -> Option<FitnessValue> {
-        if self.start == 0 {
-            Some(0)
-        } else {
-            self.start -= 1;
-            let base = (self.start / self.step + 1) * self.step;
-            let result = base + self.noise_sampler.sample(&mut self.rng);
-            Some(result as FitnessValue)
-        }
     }
 }
 
