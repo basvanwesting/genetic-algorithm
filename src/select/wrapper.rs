f67cb6d04196b909826f9bdf53fcfcebbbe5b217
basvanwesting <basvanwesting@hotmail.com>
2025-11-24 19:47:18 +0100

    remove generic helper methods from Select Wrapper (these are not intended for override)

--- src/select/wrapper.rs
+++ src/select/wrapper.rs
@@ -2,8 +2,6 @@ pub use super::elite::Elite as SelectElite;
 pub use super::tournament::Tournament as SelectTournament;
 pub use super::Select;
 
-use crate::chromosome::Chromosome;
-
 use crate::genotype::EvolveGenotype;
 use crate::strategy::evolve::{EvolveConfig, EvolveState};
 use crate::strategy::StrategyReporter;
@@ -45,44 +43,8 @@ impl<G: EvolveGenotype> Select for Wrapper<G> {
             Wrapper::Tournament(select) => select.after(genotype, state, config),
         }
     }
-
-    fn extract_elite_chromosomes(
-        &self,
-        state: &mut EvolveState<G>,
-        config: &EvolveConfig,
-        elitism_rate: f32,
-    ) -> Vec<Chromosome<G::Allele>> {
-        match self {
-            Wrapper::Elite(select) => select.extract_elite_chromosomes(state, config, elitism_rate),
-            Wrapper::Tournament(select) => {
-                select.extract_elite_chromosomes(state, config, elitism_rate)
-            }
-        }
-    }
-
-    fn parent_and_offspring_survival_sizes(
-        &self,
-        parents_size: usize,
-        offspring_size: usize,
-        target_population_size: usize,
-        replacement_rate: f32,
-    ) -> (usize, usize) {
-        match self {
-            Wrapper::Elite(select) => select.parent_and_offspring_survival_sizes(
-                parents_size,
-                offspring_size,
-                target_population_size,
-                replacement_rate,
-            ),
-            Wrapper::Tournament(select) => select.parent_and_offspring_survival_sizes(
-                parents_size,
-                offspring_size,
-                target_population_size,
-                replacement_rate,
-            ),
-        }
-    }
 }
+
 impl<G: EvolveGenotype> From<SelectElite<G>> for Wrapper<G> {
     fn from(select: SelectElite<G>) -> Self {
         Wrapper::Elite(select)
